3c5ed987579b77ec76fe0d654af34a31152d08a7
Joe Dillig <40437881+joe-at-cp@users.noreply.github.com>
2021-07-21 12:27:01 -0500

    Update cme_helper.sh
    Added debugging option for JSON output of api calls

--- cme_helper.sh
+++ cme_helper.sh
@@ -2,6 +2,7 @@
 
 #CME Helper Script for Auto Scale Automation
 
+DEBUG=false
 LOG="/var/log/cme_helper.log"
 
 . /etc/profile.d/CP.sh
@@ -14,25 +15,30 @@ echo "[ $(date +"%T")] Command: $@" >> $LOG
 if [[ $AUTOPROV_ACTION == add ]]
 then
 
-	SID=$(mgmt_cli -r true login -f json | jq -r '.sid')
-	GW_JSON=$(mgmt_cli --session-id $SID show simple-gateway name $GW_NAME -f json)
-	GW_UID=$(echo $GW_JSON | jq '.uid')
+    SID=$(mgmt_cli -r true login -f json | jq -r '.sid')
+    GW_JSON=$(mgmt_cli --session-id $SID show simple-gateway name $GW_NAME -f json)
+    if [ $DEBUG = true ]; then echo "[ DEBUG ] $GW_JSON" >> $LOG;fi
+    GW_UID=$(echo $GW_JSON | jq '.uid')
 
 	echo "[ $(date +"%T") $GW_NAME ] - Setting IDA session limit to 200k and push policy to $GW_NAME" >> $LOG
-  mgmt_cli --session-id $SID set-generic-object uid $GW_UID identityAwareBlade.iaMaxEnforcedIdentities 200000 identityAwareBlade.iaMaxAuthenticatedUsers 200000 -f json
+    JSON_OUTPUT=$(mgmt_cli --session-id $SID set-generic-object uid $GW_UID identityAwareBlade.iaMaxEnforcedIdentities 200000 identityAwareBlade.iaMaxAuthenticatedUsers 200000 -f json)
+    if [ $DEBUG = true ]; then echo "[ DEBUG ] $JSON_OUTPUT" >> $LOG;fi
 
-  echo "[ $(date +"%T") $GW_NAME ] - Setting Content Awareness Blade"  >> $LOG
-  mgmt_cli --session-id $SID set simple-gateway name $GW_NAME content-awareness true -f
+    echo "[ $(date +"%T") $GW_NAME ] - Setting Content Awareness Blade"  >> $LOG
+    JSON_OUTPUT=$(mgmt_cli --session-id $SID set simple-gateway name $GW_NAME content-awareness true -f json)
+    if [ $DEBUG = true ]; then echo "[ DEBUG ] $JSON_OUTPUT" >> $LOG;fi
 
-  echo "[ $(date +"%T") $GW_NAME ] - Publishing Session"  >> $LOG
-  mgmt_cli publish --session-id $SID -f json
+    echo "[ $(date +"%T") $GW_NAME ] - Publishing Session"  >> $LOG
+    JSON_OUTPUT=$(mgmt_cli publish --session-id $SID -f json)
+    if [ $DEBUG = true ]; then echo "[ DEBUG ] $JSON_OUTPUT" >> $LOG;fi
 
-  echo "[ $(date +"%T") $GW_NAME ] - Getting Policy Pacakge name for Gateway $GW_NAME"  >> $LOG
-  PACKAGE=$(mgmt_cli --session-id $SID show-gateways-and-servers details-level full -f json |jq --arg GWNAME "$GW_NAME" '.objects[] |select(.name==$GWNAME) | .policy."access-policy-name"')
+    echo "[ $(date +"%T") $GW_NAME ] - Getting Policy Pacakge name for Gateway $GW_NAME"  >> $LOG
+    PACKAGE=$(mgmt_cli --session-id $SID show-gateways-and-servers details-level full -f json |jq --arg GWNAME "$GW_NAME" '.objects[] |select(.name==$GWNAME) | .policy."access-policy-name"')
 
-  echo "[ $(date +"%T") $GW_NAME ] - Package Name: $PACKAGE"  >> $LOG
-  echo "[ $(date +"%T") $GW_NAME ] - Installing $PACKAGE on $GW_NAME"  >> $LOG
-  mgmt_cli --session-id $SID install-policy targets $GW_NAME policy-package $PACKAGE -f json
+    echo "[ $(date +"%T") $GW_NAME ] - Package Name: $PACKAGE"  >> $LOG
+    echo "[ $(date +"%T") $GW_NAME ] - Installing $PACKAGE on $GW_NAME"  >> $LOG
+    JSON_OUTPUT=$(mgmt_cli --session-id $SID install-policy targets $GW_NAME policy-package $PACKAGE -f json)
+    if [ $DEBUG = true ]; then echo "[ DEBUG ] $JSON_OUTPUT" >> $LOG;fi
 
-  exit 0
+    exit 0
 fi
